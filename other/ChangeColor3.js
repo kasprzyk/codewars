eaa6ef5c21bcc3dc78472ababee9d2f5d0272200
kasprzyk <piotkas1@gmail.com>
2017-08-20 15:07:37 +0200

    added kata

---
+++ other/ChangeColor3.js
@@ -0,0 +1,35 @@
+/**
+ * Created by piotr on 20.08.17.
+ */
+function zmiana(tlo) {
+    this.COLORS_RANGE = 255;
+    this.element = tlo;
+    this.colorPartial = 0;
+    this.animateForward = true;
+    this.start=function (czas) {
+        this.animationIntervalId = setInterval(function () {
+            var partial = this.colorPartial;
+            this.colorPartial+=1;
+            if (this.colorPartial%2==0){
+                this.element.style.backgroundColor = 'black';}
+            else {this.element.style.backgroundColor = 'white';}
+
+            // this.colorPartial += (this.animateForward === true ? 255 : -255);
+            //
+            // if (this.colorPartial > this.COLORS_RANGE) {
+            //     this.colorPartial = this.COLORS_RANGE;
+            //     this.animateForward = false;
+            // }
+            //
+            // if (this.colorPartial < 0) {
+            //     this.colorPartial = 0;
+            //     this.animateForward = true;
+            // }
+
+        }.bind(this), czas/2);
+
+
+    }}
+
+var anim = new zmiana(document.body);
+anim.start(3000);
