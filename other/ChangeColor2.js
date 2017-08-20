aed0b98bbb540f1b48832b5c243209590643430b
kasprzyk <piotkas1@gmail.com>
2017-08-20 15:06:50 +0200

    added kata

---
+++ other/ChangeColor2.js
@@ -0,0 +1,32 @@
+/**
+ * Created by piotr on 20.08.17.
+ */
+function zmiana(tlo) {
+    this.COLORS_RANGE = 255;
+    this.element = tlo;
+    this.colorPartial = 0;
+    this.animateForward = true;
+    this.start=function (czas) {
+        this.animationIntervalId = setInterval(function () {
+            var partial = this.colorPartial;
+            this.element.style.backgroundColor = 'rgb(' + partial + ',' + partial + ',' + partial + ')';
+
+            this.colorPartial += (this.animateForward === true ? 1 : -1);
+
+            if (this.colorPartial > this.COLORS_RANGE) {
+                this.colorPartial = this.COLORS_RANGE;
+                this.animateForward = false;
+            }
+
+            if (this.colorPartial < 0) {
+                this.colorPartial = 0;
+                this.animateForward = true;
+            }
+
+        }.bind(this), czas / 100);
+
+
+    }}
+
+var anim = new zmiana(document.body);
+anim.start(3000);
