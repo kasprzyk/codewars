9526d101ffb382f425676e164401af3d2c3e683e
kasprzyk <piotkas1@gmail.com>
2023-05-10 13:33:49 +0200

    Beeramid solution

---
+++ 5 kyu/beeramid.js
@@ -0,0 +1,13 @@
+var beeramid = (bonus, price) => {
+  let beeramidLevels = 0;
+  let beeramidPrice = price*beeramidLevels;
+  while(beeramidPrice<=bonus){
+    beeramidLevels++;
+    beeramidPrice += beeramidLevels**2*price;
+    if(beeramidPrice>bonus){
+      beeramidLevels--;
+      break
+    }
+  }
+  return beeramidLevels
+}
